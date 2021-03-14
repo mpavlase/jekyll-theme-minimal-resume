e14cffb6c4814ce030dcadcfd8cf217e19f1f3a0
Martin Pavlasek <tuzticzka@gmail.com>
2021-03-14 21:41:22 +0100

    add .travis-ci.yml and updated content

--- gulpfile.js
+++ gulpfile.js
@@ -21,26 +21,26 @@ gulp.task('jekyll-build', function (done) {
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function () {
 	browserSync.reload();
-});
+}));
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+gulp.task('browser-sync', gulp.series('jekyll-build', function() {
 	browserSync({
 		server: {
 			baseDir: '_site'
 		}
 	});
-});
+}));
 
 /*
 * Compile and minify sass
 */
 gulp.task('sass', function() {
-  	gulp.src('src/styles/**/*.scss')
+  	return gulp.src('src/styles/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(csso())
@@ -51,7 +51,7 @@ gulp.task('sass', function() {
 * Compile fonts
 */
 gulp.task('fonts', function() {
-	gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+	return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
 		.pipe(plumber())
 		.pipe(gulp.dest('assets/fonts/'));
 })
@@ -78,11 +78,12 @@ gulp.task('js', function(){
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/styles/**/*.scss', ['sass', 'jekyll-rebuild']);
-	gulp.watch('src/js/**/*.js', ['js']);
-	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', ['fonts']);
-	gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
+	// gulp.watch('src/styles/**/*.scss', gulp.series('sass', 'jekyll-rebuild'));
+	gulp.watch('src/styles/**/*.scss', gulp.series('sass'));
+	gulp.watch('src/js/**/*.js', gulp.series('js'));
+	gulp.watch('src/fonts/**/*.{tff,woff,woff2}', gulp.series('fonts'));
+	gulp.watch('src/img/**/*.{jpg,png,gif}', gulp.series('imagemin'));
+	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], gulp.series('jekyll-rebuild'));
 });
 
-gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+gulp.task('default', gulp.series('js', 'sass', 'fonts', 'browser-sync', 'watch'));
